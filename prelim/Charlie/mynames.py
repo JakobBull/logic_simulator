5e89c23c64543d70206a2b0adafc13610c871656
Charlie Marcus <charlienmarcus@gmail.com>
2021-05-15 18:09:24 +0100

    Prelim Exercise 7 started

--- prelim/Charlie/mynames.py
+++ prelim/Charlie/mynames.py
@@ -23,14 +23,19 @@ class MyNames:
                  given name ID. Returns None if the ID is not a valid index.
     """
 
-    def __init__(self):
+    def __init__(self, names_table):
         """Initialise the names list."""
+        self.names_table = []
 
     def lookup(self, name_string):
         """Return the corresponding name ID for the given name_string.
 
         If the name string is not present in the names list, add it.
         """
+        for i in range(len(self.names_table)):
+            if name_string == self.names_table[i]:
+                return i
+        self.names_table.append(name_string)
 
     def get_string(self, name_id):
         """Return the corresponding name string for the given name_id.
