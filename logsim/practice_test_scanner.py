54916f82c95195a8cec794fc50b239342d409fbe
aa2082 <aa2082@cam.ac.uk>
2021-05-28 16:44:06 +0100

    command line input now working

--- logsim/practice_test_scanner.py
+++ logsim/practice_test_scanner.py
@@ -4,6 +4,17 @@ from names import Names
 import sys
 import os
 
+
+def open_file(path):
+    try:
+        """Open and return the file specified by path for reading"""
+        return open(path, "r", encoding="utf-8")
+    except IOError:
+        print("error, can't find or open file")
+        sys.exit()
+
+
+
 def main():
     """Preliminary exercises for Part IIA Project GF2."""
 
@@ -17,8 +28,9 @@ def main():
         print("\nNow opening file...")
         # Print the path provided and try to open the file for reading
         path = os.getcwd()+ "/" + arguments[0]
+        print(path)    #print path
         scan = Scanner(path, Names())
-        print("scanning from: scanner_test_files/scanner_test_line.txt")
+        print("scanning from: " + path)
         print("")
         print("type\tid\tline#\tstart_char#\tend_char#\tstring")
         while True:
@@ -31,3 +43,6 @@ def main():
             print(symbol.string)
             if(symbol.type == scan.EOF):
                 break
+
+if __name__ == "__main__":
+    main()
