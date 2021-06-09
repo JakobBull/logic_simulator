7a87e2884357f083aa70e243363dc41f6f13870f
aa2082 <aa2082@cam.ac.uk>
2021-06-09 21:38:59 +0100

    fixed parser error numbering, all tests pass

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -15,9 +15,9 @@ def test_device_list():
     path = "parse_test_files/test_device_list.txt"
     expected_errors = [
         # [1,"SW1",5,5], opencurly called before device_list, not in device_list
-        [2,"912",6,5],
-        [3,"SW1",7,5],
-        [4,"-",8,8]
+        [2,"912"],
+        [3,"SW1"],
+        [4,"-"]
     ]
     names = Names()
     devices = Devices(names)
@@ -37,11 +37,7 @@ def test_device_list():
         str(Error.symbols[i].line_number) + " caused by symbol \"" + \
         Error.symbols[i].string + "\"" \
         + " has error type " + str(Error.types[i]) + ", should be "+  str(error[0])
-
         assert Error.symbols[i].string == error[1]
-        #assert(Error.symbols[i].line_number == error[2])
-        #assert(Error.symbols[i].start_char_number == error[3])
-
 
 @pytest.mark.parametrize(
     "file, expected_errors",
@@ -56,48 +52,40 @@ def test_device_list():
         (
             "test_1",
             [
-                [0,"CONNECTIONS",3,1]
+                [0,"CONNECTIONS"]
             ]
         ),
         (
             "test_2",
             [
-                [5,";",5,11],
-                [7,"0",7,22],
-                [6,"2",8,15],
-                [14,"SW1",12,5],
-                [20,"222",17,11],
-                [22, "}",19,3],
-                [26,"G1",22,5]
+                [7,"0"],
+                [6,"2"],
+                [22,"222"],
+                [28,"G3"]
             ]
         ),
         (
             "test_3",
             [
-                [8,"2",8,18],
-                [9,"0",9,29],
-                [16,";",12,14],
-                [17,"I2",13,14],
-                [23,"}",20,3]
+                [8,"2"],
+                [9,"0"],
+                [11,"G1Q"],
+                [16,"I5"]
             ]
         ),
         (
             "test_4",
             [
-                [0,"CONNECTIONS",3,1],
-                [1,"SW1",5,5],
-                [2,"912",6,5],
-                [3,"SW1",7,5],
-                [4,"-",8,8],
-                [11,"G1",11,5],
-                [14,"1",11,14],
-                [13,";",12,15],
-                [12,"G2.I1",13,8],
-                [11,"G22",14,5],
-                [20,"SW19",17,5],
-                [21,"0",18,9],
-                [29,"W2",22,5],
-                [26,"W2",22,5]
+                [2,"912"],
+                [4,"-"],
+                [14,"1"],
+                [16,"1"],
+                [11,"G2I2"],
+                [11,"G22"],
+                [16,"I2"],
+                [20,"SW19"],
+                [20,"SW2"],
+                [26,"W2"]
             ]
         )
 
@@ -129,21 +117,3 @@ def test_error_detection(file, expected_errors):
         + " has error type " + str(Error.types[i]) + ", should be "+  str(error[0])
 
         assert Error.symbols[i].string == error[1]
-        #assert(Error.symbols[i].line_number == error[2])
-        #assert(Error.symbols[i].start_char_number == error[3])
-
-def test_num_error_detected(file, expected_errors):
-    path = "parse_test_files/"
-    path += file
-    path += ".txt"
-
-    names = Names()
-    devices = Devices(names)
-    network = Network(names, devices)
-    monitors = Monitors(names, devices, network)
-    scanner = Scanner(path, names)
-    parser = Parser(names, devices, network, monitors, scanner)
-    parser.parse_network()
-    assert Error.num_errors == len(expected_errors), "expected " + \
-    str(len(expected_errors)) + " errors but got " + str(Error.num_errors) + \
-    " errors"
