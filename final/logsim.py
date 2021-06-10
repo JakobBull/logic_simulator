30dd6aa80b79f4da13dfcefb079da4e8c66fed05
JaBu2000 <jakob.bull@yahoo.de>
2021-06-10 14:20:18 +0100

    pep 8

--- final/logsim.py
+++ final/logsim.py
@@ -25,52 +25,16 @@ from scanner import Scanner
 from parse import Parser
 from userint import UserInterface
 from gui import Gui
-"""
-suplang = {"en" : wx.LANGUAGE_ENGLISH,
-            "de" : wx.LANGUAGE_GERMAN
-            }
-langDomain = "LOGIC SIM APP"
-
-def updateLanguage(lang):
-
-        Update the language to the requested one.
-
-        Make *sure* any existing locale is deleted before the new
-        one is created.  The old C++ object needs to be deleted
-        before the new one is created, and if we just assign a new
-        instance to the old Python variable, the old C++ locale will
-        not be destroyed soon enough, likely causing a crash.
-
-        :param string `lang`: one of the supported language codes
 
 
-        # if an unsupported language is requested default to English
-        if lang in supLang:
-            selLang = supLang[lang]
-        else:
-            selLang = wx.LANGUAGE_ENGLISH
-
-        if self.locale:
-            assert sys.getrefcount(self.locale) <= 2
-            del self.locale
-
-        # create a locale object for this language
-        self.locale = wx.Locale(selLang)
-        if self.locale.IsOk():
-            self.locale.AddCatalog(langDomain)
-        else:
-            self.locale = None
-"""
 def main(arg_list):
     """
     Parse the command line options and arguments specified in arg_list.
 
     Run either the command line user interface, the graphical user interface,
     or display the usage message.
+
     """
-
-    # Internationalisation
-
     usage_message = ("Usage:\n"
                      "Show help: logsim.py -h\n"
                      "Command line user interface: logsim.py -c <file path>\n"
@@ -81,7 +45,6 @@ def main(arg_list):
         print("Error: invalid command line arguments\n")
         print(usage_message)
         sys.exit()
-
 
     for option, path in options:
         print("option is", option, "path is", path)
@@ -116,18 +79,10 @@ def main(arg_list):
 
         Call the gui FrameManager to handle all operation.
         """
-
         language = sys.argv[-1]
 
-
-
-        # Internationalisation
-
-
-
         gui.FrameManager("Logic Simulator", language)
-        #app.MainLoop()
+
 
 if __name__ == "__main__":
-    #print(sys.argv[1])
     main(sys.argv[1:])
